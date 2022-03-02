63b35d0c7db4218bbf930fc2c1af84daaed1abf1
LittleLW8847 <thinnaphop_k@cmu.ac.th>
2022-03-02 14:17:31 +0700

    adjust script

--- test/Assets/Script/Card.cs
+++ test/Assets/Script/Card.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public int cardID;  //not really know what to do with this variable, but could be handy for identified cards
-    
-    public bool fire = false; //
-    public bool water = false;//
-    public bool earth = false;//bool variable for checking card's element.
-    public bool thunder = false;//
-    public bool dark = false;//
-    
+
+    public int cardElement = 0; // 1: Fire/ 2: Water/ 3: Earth 4: Thunder/ 5: DARK 
+    public int cardColor = 0;   // 1: Red/ 2: Blue/ 3: Brown/ 4: Yellow/ 5: urple 
+    public string elementRead;
+    public string colorRead;
     public string cardRank; 
 
     public Card()
@@ -19,17 +17,53 @@ public class Card : MonoBehaviour
 
     }
 
-    public void createCard(int ID, bool isFire, bool isWater, bool isEarth, bool isThunder, bool isDark, string rank)
+    public void createCard(int ID, int element, int colour, string rank)
     {
         cardID = ID;
-        fire = isFire;
-        water = isWater;
-        earth = isEarth;
-        thunder = isThunder;
-        dark = isDark;
+        cardElement = element;
+        cardColor = colour;
+        cardRank = rank;
+
+        switch (cardColor)
+        {
+            case 1:
+                colorRead = "Red";
+                break;
+            case 2:
+                colorRead = "Blue";
+                break;
+            case 3:
+                colorRead = "Brown";
+                break;
+            case 4:
+                colorRead = "Yellow";
+                break;
+            case 5:
+                colorRead = "Purple";
+                break;
+        }
+
+        switch (cardElement)
+        {
+            case 1:
+                elementRead = "Fire";
+                break;
+            case 2:
+                elementRead = "Water";
+                break;
+            case 3:
+                elementRead = "Earth";
+                break;
+            case 4:
+                elementRead = "Thunder";
+                break;
+            case 5:
+                elementRead = "Dark";
+                break;
+        }
     }
 
-    // Start is called before the first frame update
+    /*// Start is called before the first frame update
     void Start()
     {
         
@@ -38,6 +72,32 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        switch (cardColor)
+        {
+            case 1: colorRead = "Red";
+                break;
+            case 2: colorRead = "Blue";
+                break;
+            case 3: colorRead = "Brown";
+                break;
+            case 4: colorRead = "Yellow";
+                break;
+            case 5: colorRead = "Purple";
+                break;
+        }
+
+        switch (cardElement)
+        {
+            case 1: elementRead = "Fire";
+                break;
+            case 2: elementRead = "Water";
+                break;
+            case 3: elementRead = "Earth";
+                break;
+            case 4: elementRead = "Thunder";
+                break;
+            case 5: elementRead = "Dark";
+                break;
+        }
+    }*/
 }
